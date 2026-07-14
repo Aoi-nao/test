13d7266525cb89085dc13999a6b90491a8a00ee2
Aoi-nao <aoisuzume4869@gmail.com>
2026-07-14 20:56:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const openBtn = document.getElementById("openBtn");
+const music = document.getElementById("bgMusic");
 
 openBtn.addEventListener("click", () => {
 
-    document.body.style.transition = "opacity 1s ease";
+    music.play().catch(err => {
+        console.log(err);
+    });
 
+    document.body.style.transition = "opacity 1s ease";
     document.body.style.opacity = "0";
 
     setTimeout(() => {
-
         window.location.href = "https://withjoy.com/aoi-and-nao";
-
-    },1000);
+    }, 1500);
 
 });
